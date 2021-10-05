20a31858d600b82d19323c133c9d09b18de8c0ba
jiwoon97 <zaq0627@naver.com>
2021-10-05 15:54:31 +0900

    ㅇ

--- 15_gittest_pjw/src/main/java/project_test_20210930_jiwoon/cjw.java
+++
@@ -1,8 +0,0 @@
-package project_test_20210930_jiwoon;
-
-public class cjw {
-	
-	public static void main(String[] args) {
-		System.out.println("탈모정글러 박지운");
-	}
-}
--- GitTest_211001/src/main/java/hoho/haha.java
+++
@@ -1,9 +0,0 @@
-package hoho;
-
-public class haha {
-	public static void main(String[] args) {
-		System.out.println("Hi");
-		System.out.println("BYE");
-	
-}
-}
